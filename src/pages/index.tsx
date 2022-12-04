0b5f6d4072604ea3fd15b56ca51e3b59ff23d888
Tilo Thiele <tilo.thiele@emessages.de>
2022-12-04 22:51:35 +0100

    mit hamburg logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main style={{textAlign: 'center'}}>
       <img src="images/hzd-logo.png" alt="HZD"/>
       <Title>Hovawart Zuchtgemeinschaft Deutschland</Title>
+      <img src="images/hamburg-symbol-148x191.gif" alt="Hamburg"/>
       <h3>Ortsgruppe Hamburg</h3>
     </main>
   );
