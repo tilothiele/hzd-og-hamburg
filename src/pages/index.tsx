71fa8528cc0c35122a557387f46eac9c8e3c5990
Tilo Thiele <tilo.thiele@emessages.de>
2022-12-04 22:59:26 +0100

    mit links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,13 @@ import Title from '@/components/Title';
 export default function Home() {
   return (
     <main style={{textAlign: 'center'}}>
-      <img src="images/hzd-logo.png" alt="HZD"/>
+      <a href="https://hovawarte.com"><img src="images/hzd-logo.png" alt="HZD"/></a>
       <Title>Hovawart Zuchtgemeinschaft Deutschland</Title>
       <img src="images/hamburg-symbol-148x191.gif" alt="Hamburg"/>
-      <h3>Ortsgruppe Hamburg</h3>
+      <h3>Regionalgruppe Nord</h3>
+      <h4><a href="https://www.hovawarte.com/verein/regionalgruppen/nord/og-hamburg">Ortsgruppe Hamburg</a></h4>
+
+
     </main>
   );
 }
