5c8d5f09acf443c772f6403d0fca5f166ef0b82d
Tilo Thiele <tilo.thiele@emessages.de>
2022-12-05 07:40:28 +0100

    favicon-ect

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,36 @@ import type { GatsbyConfig } from 'gatsby';
 const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [`gatsby-plugin-pnpm`],
+  plugins: [
+    `gatsby-plugin-pnpm`,
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "HZD Ortsgruppe Hamburg",
+        short_name: "HZD OG HH",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    }
+  ],
   jsxRuntime: `automatic`,
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
-import Title from '@/components/Title';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
-    <main style={{textAlign: 'center'}}>
-      <a href="https://hovawarte.com"><img src="images/hzd-logo.png" alt="HZD"/></a>
-      <Title>Hovawart Zuchtgemeinschaft Deutschland</Title>
-      <img src="images/hamburg-symbol-148x191.gif" alt="Hamburg"/>
-      <h3>Regionalgruppe Nord</h3>
-      <h4><a href="https://www.hovawarte.com/verein/regionalgruppen/nord/og-hamburg">Ortsgruppe Hamburg</a></h4>
-
-
-    </main>
+    <Box ml={4} mr={4}>
+    <Paper sx={{textAlign: 'center', padding: 8, boxShadow: 6}} >
+      <a href="https://hovawarte.com"><StaticImage src="../images/hzd-logo.png" alt="HZD"/></a>
+      <Typography variant="h2" mt={4} mb={4}>Hovawart Zuchtgemeinschaft Deutschland</Typography>
+      <StaticImage src="../images/hamburg-symbol-148x191.gif" alt="Hamburg"/>
+      <Typography variant="h3" mt={4}>Regionalgruppe Nord</Typography>
+      <Typography variant="h4" mb={4}><a href="https://www.hovawarte.com/verein/regionalgruppen/nord/og-hamburg">Ortsgruppe Hamburg</a></Typography>
+      <Typography>Die Email-Adresse unserer Geschäftsstelle lautet geschaeftsstelle@hzd-og-hamburg.de.</Typography>
+      <Typography>Weitere Infos, Impressum und Kontaktmöglichkeiten finden Sie auf der
+        Haupt-Webpräsenz <a href="https://www.hovawarte.com/verein/regionalgruppen/nord/og-hamburg/ansprechpartner">des HZD</a>.</Typography>
+    </Paper>
+    </Box>
   );
 }
