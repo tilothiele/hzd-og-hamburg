b9b41bec4dbd431f7ceac262a906b971e2448dd0
tilothiele <tilothiele@users.noreply.github.com>
2023-11-06 05:11:59 +0000

    Deploying to gh-pages from @ tilothiele/hzd-og-hamburg@e15cad23b48af71766fe961391c0894911bdc9cb 🚀

---
+++ component---src-pages-404-tsx-7a1c7eaddac5dd08bbc8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_ts=self.webpackChunkgatsby_starter_ts||[]).push([[218],{50:function(t,n,r){r.r(n),r.d(n,{default:function(){return s}});var e=r(1527);function s(){return(0,e.jsx)("main",{children:(0,e.jsx)("p",{children:"Sorry, page not found!"})})}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-7a1c7eaddac5dd08bbc8.js.map
--- src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <main>
-      <p>Sorry, page not found!</p>
-    </main>
-  );
-}
